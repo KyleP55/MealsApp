54656d7282b6797666f2c494559f4f98af84c0c8
Kyle P <powell-5@hotmail.com>
2023-11-19 21:44:36 -0500

    meal page alternated

--- pages/MealPage.js
+++ pages/MealPage.js
@@ -1,5 +1,13 @@
 import { Text, View, Image, StyleSheet } from "react-native";
 
-function MealPage() { };
+function MealPage() {
+    return (<>
+        <View>
+            <Text>Meal Title</Text>
+            <Text>Categories</Text>
+            <Text>Recipes</Text>
+        </View>
+    </>)
+};
 
 export default MealPage;
